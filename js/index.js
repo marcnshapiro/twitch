ca74fa57455006f1aea3b1d9e4fd940beb45355d
Marc Shapiro <marcnshap@gmail.com>
2016-09-08 23:45:36 -0700

    Upload for comments

--- js/index.js
+++ js/index.js
@@ -1,71 +1,88 @@
-var link = "";
 var status = "";
 var logo = "";
 var user = "";
 var display_name = "";
 var game = "";
+var link = "";
+var html="";
 
-var users = [];
+var users = ["OgamingSC2",
+             "freecodecamp",
+             "brunofin"];
 
 $(document).ready( function() {
-  user = "OgamingSC2";
-  //user = "freecodecamp";
-  //user = "brunofin";
   
-    alert("Getting JSON");
+  for (var i = 0; i < users.length; i++)
+    getData(users[i], i);
+    //users.forEach(getData);
+  
+  function getData(user, index) {
+    var streamURL = 'https://api.twitch.tv/kraken/streams/' + user + '?callback=?';
+    var channelURL = 'https://api.twitch.tv/kraken/channels/' + user + '?callback=?';
 
-  $.getJSON('https://api.twitch.tv/kraken/streams/' + user + '?callback=?', function(dataStream) { 
-    alert(dataStream.error);
+    var online = "False";
+    var active = "False";
+    var display_name = "";
+    var game = "";
+    var logo = "";
+
+    $.getJSON(streamURL, function(dataStream) { 
+
+      alert("");
 
       if (dataStream.error) {
-        $("#online").html("Online: False");
-        $("#active").html("Active: False");
-        link = "";
-      } else if (dataStream.stream) {
-        $("#online").html("Online: True");
-        $("#active").html("Active: True");
-        $.getJSON('https://api.twitch.tv/kraken/channels/' + user + '?callback=?', function(dataChannel) {
-          if (!dataChannel.url === "") {
-            link = dataChannel.url + "' target='_blank'";
-          } else {
-            link = "";
-          }
-          display_name = dataChannel.display_name;
-          game = dataChannel.game;
-        }
-        
-      } else {
-        $("#online").html("Online: False");
-        $("#active").html("Active: True");
-        link = "";
-      }
-    
-      if (link != "") {
-        $("#link").html("Link: <a href=" + link + ">" + user + "</a>");
-        $("#display_name").html(display_name);
-        $("#game").html("Link: <a href=" + link + ">" + user + "</a>");
-      } else {
-        $("#link").html("None");
-        $("#display_name").html("None");
-        $("#game").html("None");
-      }
-    
-    
-    //if (data.stream) {
-    //  name = data.stream.channel.display_name;
-    //  link = data.stream.channel.url;
-    //  status = data.stream.channel.status;
-    //  logo = data.stream.channel.logo
-    //} else {
-    //  name = data.display_name;
-    //  link = data.url;
-    //  status = "Offline";
-    //  logo = data.logo
-    //}
-  });
-  
-  $("#changeUnits").on("click", function() {
-    window.open(link);
-  });
 
+        online = "False";
+        active = "False";
+        display_name = user;
+        logo = "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F"
+        game = "Account Closed";
+
+        html = '<div class="row">' +
+        '<div class="col-xs-2 col-sm-1" id="icon">' + '<img src="' + logo + '" height=50 width=50"></div>' +
+        '<div class="col-xs-5 col-sm-3" id="name">' + display_name + '</div>' + 
+        '<div class="col-xs-5 col-sm-8" id="streaming">'+ game + '</div>' +
+        '</div>';
+      } else {
+        if (dataStream.stream) {
+          online = "True";
+          active = "True";
+        } else {
+          online = "False";
+          active = "True";
+        }
+
+        $.getJSON(channelURL, function(dataChannel) {
+
+          if (dataChannel.url != "") {
+            if (dataChannel.logo != null) {
+              logo = dataChannel.logo;
+            } else {
+              logo = "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F"
+            }
+
+            display_name = dataChannel.display_name;
+
+            if (online === "True") {
+              game = dataChannel.game ;
+            } else {
+              game = "Offline";
+            }
+
+            html = '<div class="row">' +
+            '<div class="col-xs-2 col-sm-1" id="icon">' + '<img src="' + logo + '" height=50 width=50"></div>' +
+            '<div class="col-xs-5 col-sm-3" id="name"><a href="' + dataChannel.url + '" target="_blank">' + display_name + '</a></div>' + 
+            '<div class="col-xs-5 col-sm-8" id="streaming">'+ game + '</div>' +
+            '</div>';
+          }
+        });
+
+      }
+
+      alert("");
+
+      $("#display").append(html);
+
+    });
+  }
 });
